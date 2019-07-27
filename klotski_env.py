f2950e39651963c52f3219fcf91ceb513c7d852e
Arpit Garg <iarpitgarg@gmail.com>
2019-07-27 12:32:37 -0600

    cell encoding moved to Piece class

--- klotski_env.py
+++ klotski_env.py
@@ -1,5 +1,5 @@
 import numpy as np
-from piece import Piece
+from piece import Piece, EMPTY_CELL_ID, DAUGHTER_PIECE_IDS
 from gym.spaces import Box, Discrete
 from utils import setup_logger
 import gym
@@ -33,18 +33,18 @@ class KlotskiEnv(gym.Env):
         # check if valid move
         current_cells = self.pieces[piece_id].get_occupied_cells()
         new_cells = self.pieces[piece_id].get_new_occupied_cells(action_direction)
-        new_delta_cells = new_cells - current_cells
-        old_delta_cells = current_cells - new_cells
-        is_valid_move = len(new_delta_cells) == sum([int(0 <= cell[0] < HEIGHT and
-                                                         0 <= cell[1] < WIDTH and
-                                                         self.state[cell[0]][cell[1]] == Piece.EMPTY_CELL_ID)
-                                                     for cell in new_delta_cells])
+        new_vertex_cells = new_cells - current_cells
+        old_vertex_cells = current_cells - new_cells
+        is_valid_move = len(new_vertex_cells) == sum([int(0 <= cell[0] < HEIGHT and
+                                                          0 <= cell[1] < WIDTH and
+                                                          self.state[cell[0]][cell[1]] == EMPTY_CELL_ID)
+                                                      for cell in new_vertex_cells])
 
         # update game state
         if is_valid_move:
             self.pieces[piece_id].step(action_direction)
-            self.mark_cells(new_delta_cells, piece_id)
-            self.mark_cells(old_delta_cells, Piece.EMPTY_CELL_ID)
+            self.mark_cells(new_vertex_cells, piece_id)
+            self.mark_cells(old_vertex_cells, EMPTY_CELL_ID)
 
         # check if terminal condition and set reward
         if self._step_cnt >= MAX_STEPS:
@@ -53,8 +53,8 @@ class KlotskiEnv(gym.Env):
         else:
             if not is_valid_move:
                 reward = -1
-            elif (self.state[3][1] == Piece.DAUGHTER_PIECE_ID and self.state[3][2] == Piece.DAUGHTER_PIECE_ID and
-                  self.state[4][1] == Piece.DAUGHTER_PIECE_ID and self.state[4][2] == Piece.DAUGHTER_PIECE_ID):
+            elif (self.state[3][1] == DAUGHTER_PIECE_IDS[0] and self.state[3][2] == DAUGHTER_PIECE_IDS[0] and
+                  self.state[4][1] == DAUGHTER_PIECE_IDS[0] and self.state[4][2] == DAUGHTER_PIECE_IDS[0]):
                 reward = 10
                 done = True
 
@@ -71,7 +71,7 @@ class KlotskiEnv(gym.Env):
             self.mark_cells(piece.get_occupied_cells(), piece_id)
 
         # mark empty cells
-        self.mark_cells(Piece.EMPTY_CELLS, Piece.EMPTY_CELL_ID)
+        self.mark_cells(Piece.get_empty_cells(), EMPTY_CELL_ID)
 
         return self.get_state()
 
@@ -88,33 +88,20 @@ class KlotskiEnv(gym.Env):
     def render(self, mode='human'):
         from gym.envs.classic_control import rendering
         sf = 100
-        margin = 0.1
+        margin = 0.05
 
         if self.viewer is None:
             self.viewer = rendering.Viewer(WIDTH*sf, HEIGHT*sf)
-
-        # colors = [(19, 124, 201), (171, 209, 217), (234, 203, 253), (253, 126, 127)]
-        colors = [(0, 0, 255), (0, 255, 0), (139, 0, 139), (255, 0, 0)]
-        color_mapping = {7: 0, 4: 0, 9: 0, 6: 0, 0: 1, 1: 1, 2: 1, 3: 1, 5: 2, 8: 3}
 
         for piece_id in range(NUM_PIECES):
             cells = list(self.pieces[piece_id].get_occupied_cells())
             cells.sort()
             cell = cells[0]
-            color = colors[color_mapping[piece_id]]
-
-            if piece_id in [7, 4, 9, 6]:
-                deltas = [(0, 0), (0, 1), (2, 1), (2, 0)]
-            elif piece_id in [0, 1, 2, 3]:
-                deltas = [(0, 0), (0, 1), (1, 1), (1, 0)]
-            elif piece_id == 5:
-                deltas = [(0, 0), (0, 2), (1, 2), (1, 0)]
-            else:
-                deltas = [(0, 0), (0, 2), (2, 2), (2, 0)]
-
+            color = self.pieces[piece_id].get_color()
+            vertices = self.pieces[piece_id].get_vertices()
             margins = [(margin, margin), (margin, -margin), (-margin, -margin), (-margin, margin)]
-            deltas = [(delta[0]+margin[0], delta[1]+margin[1]) for delta, margin in list(zip(deltas, margins))]
-            vertices = [(cell[0] + delta[0], cell[1] + delta[1]) for delta in deltas]
+            vertices = [(vertex[0]+margin[0], vertex[1]+margin[1]) for vertex, margin in list(zip(vertices, margins))]
+            vertices = [(cell[0] + vertex[0], cell[1] + vertex[1]) for vertex in vertices]
             vertices = [(v[1]*sf, (HEIGHT-v[0])*sf) for v in vertices]
             self.viewer.draw_polygon(vertices, color=color)
 
--- piece.py
+++ piece.py
@@ -1,11 +1,26 @@
+# Piece encoding
+EMPTY_CELL_ID = 10
+SERVANTS_PIECE_IDS = [0, 1, 2, 3]
+HORIZONTAL_PIECE_IDS = [5]
+VERTICAL_PIECE_IDS = [4, 6, 7, 9]
+DAUGHTER_PIECE_IDS = [8]
+
+# All types of pieces and their color and vertices mapping
+PIECE_TYPES = [SERVANTS_PIECE_IDS, HORIZONTAL_PIECE_IDS, VERTICAL_PIECE_IDS, DAUGHTER_PIECE_IDS]
+PIECE_COLORS = [(0, 255, 0), (139, 0, 139), (0, 0, 255), (255, 0, 0)]
+PIECE_VERTICES = [[(0, 0), (0, 1), (1, 1), (1, 0)],
+                  [(0, 0), (0, 2), (1, 2), (1, 0)],
+                  [(0, 0), (0, 1), (2, 1), (2, 0)],
+                  [(0, 0), (0, 2), (2, 2), (2, 0)]]
+
+
 class Piece:
-    EMPTY_CELL_ID = 10
-    DAUGHTER_PIECE_ID = 8
-    EMPTY_CELLS = [(4, 1), (4, 2)]
     ACTION_DELTAS = {0: (-1, 0), 1: (0, 1), 2: (0, -1), 3: (1, 0)}
 
-    def __init__(self, cells):
+    def __init__(self, cells, color, vertices):
         self._occupied_cells = set(cells)
+        self.color = color
+        self.vertices = vertices
 
     @staticmethod
     def init_piece(piece_id):
@@ -19,28 +34,43 @@ class Piece:
         :param piece_id: ID of the piece as shown above
         :return: A Piece object
         """
+
+        # get occupied cells
         if piece_id == 0:
-            return Piece([(4, 0)])
+            cells = [(4, 0)]
         elif piece_id == 1:
-            return Piece([(3, 1)])
+            cells = [(3, 1)]
         elif piece_id == 2:
-            return Piece([(3, 2)])
+            cells = [(3, 2)]
         elif piece_id == 3:
-            return Piece([(4, 3)])
+            cells = [(4, 3)]
         elif piece_id == 4:
-            return Piece([(2, 0), (3, 0)])
+            cells = [(2, 0), (3, 0)]
         elif piece_id == 5:
-            return Piece([(2, 1), (2, 2)])
+            cells = [(2, 1), (2, 2)]
         elif piece_id == 6:
-            return Piece([(2, 3), (3, 3)])
+            cells = [(2, 3), (3, 3)]
         elif piece_id == 7:
-            return Piece([(0, 0), (1, 0)])
+            cells = [(0, 0), (1, 0)]
         elif piece_id == 8:
-            return Piece([(0, 1), (0, 2), (1, 1), (1, 2)])
+            cells = [(0, 1), (0, 2), (1, 1), (1, 2)]
         elif piece_id == 9:
-            return Piece([(0, 3), (1, 3)])
+            cells = [(0, 3), (1, 3)]
         else:
             raise ValueError("Undefined piece ID!")
+
+        # get color
+        color = None
+        for piece_type in PIECE_TYPES:
+            if piece_id in piece_type:
+                color = PIECE_COLORS[PIECE_TYPES.index(piece_type)]
+                vertices = PIECE_VERTICES[PIECE_TYPES.index(piece_type)]
+
+        return Piece(cells, color, vertices)
+
+    @staticmethod
+    def get_empty_cells():
+        return [(4, 1), (4, 2)]
 
     def step(self, action):
         """
@@ -54,6 +84,12 @@ class Piece:
 
     def get_occupied_cells(self):
         return self._occupied_cells
+
+    def get_color(self):
+        return self.color
+
+    def get_vertices(self):
+        return self.vertices
 
     def get_new_occupied_cells(self, action):
         dx, dy = self.ACTION_DELTAS[action]
